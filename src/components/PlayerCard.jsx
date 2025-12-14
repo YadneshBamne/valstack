7b415fcecc1cca16278a58e573fb470efaa3647f
YadneshBamne <yadnesh2105@gmail.com>
2025-12-15 03:33:55 +0530

    added icons

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -163,11 +163,9 @@ export default function PlayerCard({ player }) {
                 </div>
                 {player.account_level > 0 && (
                   <div className="flex items-center gap-1 px-2 py-0.5 bg-[#2a3a47] rounded">
-                    <svg className="w-3 h-3 text-[#00d4aa]" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
-                    </svg>
-                    <span className="text-[#7a8a99] text-xs font-mono">{player.account_level}</span>
-                  </div>
+            <Trophy className="w-3 h-3 text-[#00d4aa]" />
+            <span className="text-[#7a8a99] text-xs font-mono">{player.account_level}</span>
+          </div>
                 )}
               </div>
 
